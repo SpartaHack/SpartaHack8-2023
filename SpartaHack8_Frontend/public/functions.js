cef9413114873566525e64a2900a25a8988f23fb
joelnataren9 <joelnataren9@hotmail.com>
2022-10-29 00:31:45 -0400

    Fixed the bug of sending emails with the form and added the sent and loading messages without css

--- SpartaHack8_Frontend/public/functions.js
+++ SpartaHack8_Frontend/public/functions.js
@@ -1,14 +1,14 @@
-window.document.onload = function(e){ 
-    function handleForm(event) { event.preventDefault(); } 
+window.document.onload = function(e){
+    function handleForm(event) { event.preventDefault(); }
     document.getElementById("submit").addListener("click", add_email);
     document.getElementById("send_emails").addListener("click", send_curr_email);
 }
 
 async function add_email(e){
-    // e.preventDefault()
+    e.preventDefault()
+    document.getElementById("email_loading").style.display = "block";
 	const email = document.getElementById("email").value;
     console.log(email);
-
     const response = await fetch('https://us-central1-spartahack8.cloudfunctions.net/add_email', {
     method: 'POST',
     body:  JSON.stringify({"email": email}),
@@ -18,36 +18,43 @@ async function add_email(e){
     });
     const myJson = await response.json();
     console.log(myJson);
+    document.getElementById("email_loading").style.display = "None";
+    document.getElementById("email_sent").style.display = "block";
+
+    setTimeout(() => {
+        document.getElementById("email_sent").style.display = "None";
+    }, 3000);
+
 }
+
 function submit_email(e){
-    // e.preventDefault(); 
+    // e.preventDefault();
     add_email()
 }
 
-async function send_emails(e) {
-    const id = "1233";
-    const name = "Joel Nataren";
-    const email = "joelnataren9@hotmail.com";
-    const message =`Hola, sos un crack`;
+// async function send_emails(e) {
+//     const id = "1233";
+//     const name = "Joel Nataren";
+//     const email = "joelnataren9@hotmail.com";
+//     const message =`Hola, sos un crack`;
 
-    const response = await fetch("https://us-central1-spartahack8.cloudfunctions.net/send_emails", {
-        method: 'POST',
-        body: JSON.stringify({
-            data: {
-                id : id,
-                name: name,
-                email: email,
-                message: message,
-            },  
-        },
-        headers: {
-            'Content-Type': 'application/json'
-        });
-        const myJson = await response.json();
-        console.log(myJson);
-    });
-
-}
+//     const response = await fetch("https://us-central1-spartahack8.cloudfunctions.net/send_emails", {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             "data": {
+//                 id : id,
+//                 name: name,
+//                 email: email,
+//                 message: message,
+//             }
+//         }),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     });
+//     const myJson = await response.json();
+//     console.log(myJson);
+// }
 
 function send_curr_email(e) {
     send_emails();
