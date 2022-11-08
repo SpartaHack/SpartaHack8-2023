9e7c3be7523f095618d4430a7bfdb0a284d7f58f
LeoSSpecht <62398409+LeoSSpecht@users.noreply.github.com>
2022-11-08 14:36:49 -0500

    Unit testing for functions

--- SpartaHack8_Backend/registrations/functions/index.js
+++ SpartaHack8_Backend/registrations/functions/index.js
@@ -1,4 +1,16 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+require("firebase-functions/logger/compat");
+admin.initializeApp();
+const db = admin.firestore();
+const cors = require('cors')({origin: true});
+
+// Missing fields
+// Minor with forms
+// Minor with no forms
+// Normal
+// Msu student
+// Non msu student
 
 const required_fields = [
 "email",
@@ -19,8 +31,8 @@ const required_fields = [
 
 function fields_validation(data,required_fields){
 	for(field_i in required_fields){
-		field = required_fields[field_i];
-		if(!(field in data)):{
+		var field = required_fields[field_i];
+		if(!(field in data)){
 			return [false, field];
 		}
 	}
@@ -28,7 +40,7 @@ function fields_validation(data,required_fields){
 }
 
 function calculate_age(birth){
-	var diff = Math.floor(new Date().getTime() - date2.getTime());
+	var diff = Math.floor(new Date().getTime() - birth.getTime());
     var day = 1000 * 60 * 60 * 24;
     var days = Math.floor(diff/day);
     var months = Math.floor(days/31);
@@ -40,32 +52,36 @@ exports.registeUser = functions.https.onRequest((request, response) => {
   response.set({ 'Access-Control-Allow-Origin': '*' })
     cors(request, response, () => {
         if(request.method === "POST"){
-        	let required_fields = [];
-            var postData = request.body;
-            var all_fields_valid = fields_validation(postData,required_fields);
-            if(all_fields_valid[0]){
-            	// Check date of birth
-            	let birth_date = postData['date_of_birth'];
-            	postData["accepted_policy"] = true;
-            	postData["approved"] = true;
-            	if(calculate_age(birth_date) < 18){
-            		if(!("content_form" in postData)){
-            			response.status(500).send({"message":`Missing consent form for minor`});
-            		}
-            		postData["approved"] = false;
-            		postData["minor"] = true;
-            	}
-            	let is_msu_student = postData["email"].includes("@msu.edu");
-            	if is_msu_student{
-            		postData["msu_student"] = true;
-            		let len = postData["email"].search("@")+1;
-            		postData["net_id"] = postData["email"].substr(0,len);
-            	}
-            	response.status(200).send({"message":"sucess", "data": postData});
-            }
-            else{
-            	response.status(500).send({"message":`Missing ${all_fields_valid[1]} parameter`});
-            }
+          var postData = request.body;
+          var all_fields_valid = fields_validation(postData,required_fields);
+          if(all_fields_valid[0]){
+          	// Check date of birth
+          	let birth_date = new Date(postData['date_of_birth']);
+            postData['date_of_birth'] = birth_date;
+            postData['graduation_date'] = new Date(postData['graduation_date']);
+          	postData["accepted_policy"] = true;
+          	postData["approved"] = true;
+            postData["minor"] = false;
+          	if(calculate_age(birth_date) < 18){
+          		if(!("content_form" in postData)){
+          			response.status(500).send({"message":`Missing consent form for minor`});
+          		}
+          		postData["approved"] = false;
+          		postData["minor"] = true;
+          	}
+          	let is_msu_student = postData["email"].includes("@msu.edu");
+            postData["msu_student"] = false;
+            postData["net_id"] = null;
+          	if(is_msu_student){
+          		postData["msu_student"] = true;
+          		let len = postData["email"].search("@")+1;
+          		postData["net_id"] = postData["email"].substr(0,len-1);
+          	}
+          	response.status(200).send({"message":"sucess", "data": postData});
+          }
+          else{
+          	response.status(500).send({"message":`Missing ${all_fields_valid[1]} parameter`});
+          }
 
         }
         else response.status(500).send({"message":"Please send a post request"});
---
+++ SpartaHack8_Backend/registrations/run_test.sh
@@ -0,0 +1,1 @@
+python3 unit_tests.py -v
