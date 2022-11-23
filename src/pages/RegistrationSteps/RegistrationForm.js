cc398cfe7275c2ea204b535aed4a206c426b7a9e
joelnataren9 <joelnataren9@hotmail.com>
2022-11-23 12:04:14 -0500

    mend

--- src/pages/RegistrationSteps/RegistrationForm.js
+++ src/pages/RegistrationSteps/RegistrationForm.js
@@ -162,7 +162,9 @@ function RegistrationForm() {
       setMessageInfo("An error occurred. Please review your form and try again or contact us if there is an issue");
       setShowModal(true);
     }
+    console.log(myJson);
     console.log(myJson.message);
+
     // console.log(url)
     return myJson.message
   }
