9b08e6dbdf8d3ff8cfbdeb5d3c583fa593382c65
Mann Aswal <mann1aswal@gmail.com>
2022-12-02 19:19:03 -0500

    Modified footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -4,12 +4,20 @@ import LinkIcon from '../icons/LinkIcon'
 function Footer() {
   return (
     <div>
-      <footer className='w-full h-32 flex flex-col justify-center items-center'>
-        <span className='whitespace-pre-wrap'> 
-    <a
+      <footer className='w-full h-32'>
+        <div className='h-full mx-4 md:mx-auto md:max-w-xl lg:max-w-4xl xl:max-w-5xl mx-auto flex flex-row justify-between items-center'>
+        <div className='text-sh-white/30'><span>
+          Contact us at <span className='select-all cursor-pointer text-sky-500/50'>hello@spartahack.com</span>
+        </span>
+        </div>
+        <div>
+        <span className='whitespace-pre-wrap'><a
       href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target="_blank" rel="noopener noreferrer"
-      className='text-sh-white/30 text-lg'>MLH Code of Conduct</a >
-  </span>
+      className='text-sh-white/30'>MLH Code of Conduct</a >
+        </span>
+        </div>
+        </div>
+        
       </footer>
     </div>
   )
