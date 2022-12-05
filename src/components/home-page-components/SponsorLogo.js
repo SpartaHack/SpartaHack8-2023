83d8485f777d215c7b76dcf3d1dc83d143719241
Mann Aswal <mann1aswal@gmail.com>
2022-12-05 03:15:33 -0500

    fixed bugs in Sponsors section

--- src/components/home-page-components/SponsorLogo.js
+++ src/components/home-page-components/SponsorLogo.js
@@ -3,7 +3,7 @@ import React from 'react'
 function SponsorLogo(props) {
   return (
     <div className={" flex flex-col justify-center items-center bg-white/90 backdrop-blur-md rounded-xl"}>
-      <img src={props.path} alt={props.altText} className={props.class + " max-h-fit md:max-h-24 my-auto max-w-[200px] md:max-w-[300px] p-4"} />
+      <img src={props.path} alt={props.altText} className={props.class + " max-h-20 sm:max-h-fit lg:max-h-24 my-auto max-w-[150px] sm:max-w-[200px] lg:max-w-[300px] p-4"} />
     </div>
   )
 }
--- src/components/home-page-components/SponsorsLogos.js
+++ src/components/home-page-components/SponsorsLogos.js
@@ -4,7 +4,7 @@ import SponsorLogo from './SponsorLogo'
 function SponsorsLogos(props) {
 
   return (
-    <div className='flex flex-row flex-wrap justify-center items-stretch gap-6 '>
+    <div className='flex flex-row flex-wrap justify-center items-stretch gap-4 md:gap-6 '>
       {props.logos.map((logo) => {
         return (
           <SponsorLogo path={logo.path} altText={logo.name} class={(logo.class) ? logo.class : ""} />
