d12c3e576079215b22ca74d9df978638b639d4e2
achyutbenz19 <achyut.benz@gmail.com>
2023-11-02 12:33:31 -0400

    minor

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,12 @@
+import SignUp from '@/components/auth/signup'
+import React from 'react'
+
+const SignUpPage = () => {
+  return (
+    <div>
+        <SignUp/>
+    </div>
+  )
+}
+
+export default SignUpPage
