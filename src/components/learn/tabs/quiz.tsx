bed4542bf93c9e3e68c7d41ec9b435d4e07bea2d
achyutbenz19 <achyut.benz@gmail.com>
2023-11-03 15:39:06 -0400

    minor tabs

--- src/components/learn/tabs/quiz.tsx
+++ src/components/learn/tabs/quiz.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useContainerHeight } from '@/hooks/use-container-height';
+import { ScrollShadow } from '@nextui-org/react';
+import { CustomButton } from '@/helpers/custom-btn';
 
 const Quiz = () => {
+  const height = useContainerHeight();
   return (
-    <div>Quiz</div>
+    <>
+      <div style={{minHeight: `${height-190}px`}}>
+        <ScrollShadow size={5} hideScrollBar className='flex-grow overflow-hidden overflow-y-auto rounded-lg text-[50px]' style={{maxHeight: `${height-140}px`}}>
+          TEXT FOR DUMMIES sadad
+          TEXT FOR DUMMIES
+          TEXT FOR DUMMIES
+          TEXT FOR DUMMIES
+          TEXT FOR DUMMIES
+        </ScrollShadow>
+      </div>
+      <div className="mt-3 w-full">
+        <CustomButton
+          btnStyling='w-full bg-secondary text-black'
+          title='Submit Quiz'
+          btnType='submit'
+        />
+    </div>
+    </>
   )
 }
 
--- src/components/learn/tabs/summary.tsx
+++ src/components/learn/tabs/summary.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useContainerHeight } from '@/hooks/use-container-height';
+import { ScrollShadow } from '@nextui-org/react';
 
 const Summary = () => {
+  const height = useContainerHeight();
   return (
-    <div>Summary</div>
+    <div style={{minHeight: `${height-190}px`}}>
+      <ScrollShadow size={5} hideScrollBar className='flex-grow overflow-hidden overflow-y-auto rounded-lg text-[50px]' style={{maxHeight: `${height-100}px`}}>
+        TEXT FOR DUMMIES sadad
+        TEXT FOR DUMMIES
+        TEXT FOR DUMMIES
+        TEXT FOR DUMMIES
+        TEXT FOR DUMMIES
+      </ScrollShadow>
+    </div>
   )
 }
 
