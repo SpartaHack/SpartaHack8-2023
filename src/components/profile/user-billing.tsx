2810142bd9cbc2435075bfd92570273d7650b298
achyutbenz19 <achyut.benz@gmail.com>
2023-11-15 21:58:22 -0500

    minor

--- src/components/profile/user-billing.tsx
+++ src/components/profile/user-billing.tsx
@@ -8,6 +8,34 @@ const UserBilling = () => {
           Billing & Payments
       </h1>
       <div className='horizontal-line mt-3 mb-10'/>
+      <div className='flex flex-col'>
+        <div className='flex flex-row justify-between'>
+          <div className='flex flex-row'>
+            <h1 className='text-xl mt-2 ml-4'>Subscription Plan</h1>
+            <div className='flex flex-col ml-10 lg:ml-[70px] md:ml-20'>
+              <h2 className='mt-2.5'>Pro Plan - $10 a month</h2>
+              <h2 className='mt-2.5 text-sm'>Plan ends December 10, 2023</h2>
+            </div>
+          </div>
+          <CustomButton title='Edit Plan' btnType='button' btnStyling='hidden md:block mt-2'/>
+        </div>
+        <div className='flex mt-8 flex-row justify-between'>
+          <div className='flex flex-row'>
+            <h1 className='text-xl mt-2 ml-4'>Payment & Billing</h1>
+            <div className='flex flex-col lg:flex-row'>
+              <div className='flex flex-col ml-10 lg:ml-[70px] md:ml-20'>
+                <h2 className='mt-2.5'>Payment Information</h2>
+                <h2 className='mt-2.5 text-sm'>42424242424242</h2>
+              </div>
+              <div className='flex flex-col mt-3 lg:mt-0 ml-10 lg:ml-[70px] md:ml-20'>
+                <h2 className='mt-2.5'>Billing Information</h2>
+                <h2 className='mt-2.5 text-sm'>123 Street<br/>Seoul, Bangalore</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <CustomButton title='Edit Plan' btnType='button' btnStyling='w-full w-full md:hidden mt-6'/>
     </div>
   )
 }
