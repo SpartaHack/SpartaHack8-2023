61d3ba537fd6f6e596de43e4d806acaee7f908b5
achyutbenz19 <achyut.benz@gmail.com>
2023-11-17 15:00:32 -0500

    minor

--- src/ui/header/header.tsx
+++ src/ui/header/header.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from "react";
+import React, { useRef, useState } from "react";
 import YouLearnLogo from "@/icon/youlearn-logo";
 import BetaLogo from "@/icon/beta-logo";
 import HomeLinkBar from "./home-link-bar";
@@ -8,10 +8,20 @@ import Account from "./account";
 import Notification from "./notification";
 import MenuItems from "./side-menu/menu-items";
 import { sideBarMotion } from "../../../utils";
+import useClickOutside from "@/hooks/use-click-outside";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useClickOutside(ref, () => setIsMenuOpen(false));
+
+  const handleClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+  };
+
   return (
-    <Navbar maxWidth="full" shouldHideOnScroll isBordered className="sticky top-0 z-10 bg-neutral-100 dark:bg-neutral-900 border-b border-neutral-300 dark:border-neutral-700">
+    <Navbar maxWidth="full" isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen} shouldHideOnScroll isBordered className="sticky top-0 z-10 bg-neutral-100 dark:bg-neutral-900 border-b border-neutral-300 dark:border-neutral-700">
       <div className="flex w-full py-3 items-center justify-between">
         <div className='md:mr-6 mr-5 h-8'>
           <NavbarMenuToggle/>
@@ -28,9 +38,13 @@ export default function Header() {
         </div>
       </div>
 
-      <NavbarMenu className='w-[50%] md:w-[25%] lg:w-[18%] bg-white dark:bg-neutral-900 border-t dark:border-neutral-800 ' motionProps={sideBarMotion}>
-        <MenuItems/>
-      </NavbarMenu>
+      <div onClick={handleClick}>
+        <NavbarMenu className='w-[50%] md:w-[25%] overflow-hidden lg:w-[18%] bg-white dark:bg-neutral-900 border-t dark:border-neutral-800 ' motionProps={sideBarMotion}>
+          <div ref={ref}>  
+            <MenuItems/>
+          </div>
+        </NavbarMenu>
+      </div>
 
     </Navbar>
   );
