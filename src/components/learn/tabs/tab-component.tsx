31984a7c1970593ff0dd1dcad69774cb9a5712d5
achyutbenz19 <achyut.benz@gmail.com>
2023-11-18 13:14:21 -0500

    tabs color

--- src/components/learn/tabs/tab-component.tsx
+++ src/components/learn/tabs/tab-component.tsx
@@ -8,7 +8,7 @@ const TabComponent = () => {
 
   return (
     <div className="flex flex-col w-full h-full p-3 overflow-hidden">
-      <Tabs fullWidth>
+      <Tabs variant="light" fullWidth className="border rounded-xl dark:border-neutral-700">
           <Tab title="Chat" className="h-full dark:bg-neutral-800">
             <Chat/>
           </Tab>
--- src/ui/header/side-menu/menu-items.tsx
+++ src/ui/header/side-menu/menu-items.tsx
@@ -6,7 +6,7 @@ import MenuFooter from '@/ui/footer/menu-footer'
 
 const MenuItems = () => {
   return (
-    <div className='flex flex-col h-[92vh] space-y-4'>
+    <div className='flex flex-col h-[90vh] space-y-4'>
       <div className='flex flex-row w-full lg:hidden cursor-pointer hover:bg-neutral-200 dark:hover:bg-neutral-700 p-2 rounded-xl'>
         <Notification/>
       </div>
