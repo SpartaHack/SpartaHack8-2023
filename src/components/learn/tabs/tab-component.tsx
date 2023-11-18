e940650adb40fda76209b47de3dcf375d8df1375
achyutbenz19 <achyut.benz@gmail.com>
2023-11-18 13:20:50 -0500

    tabs color

--- src/components/learn/tabs/tab-component.tsx
+++ src/components/learn/tabs/tab-component.tsx
@@ -8,16 +8,16 @@ const TabComponent = () => {
 
   return (
     <div className="flex flex-col w-full h-full p-3 overflow-hidden">
-      <Tabs variant="light" fullWidth className="border rounded-xl dark:border-neutral-700">
-          <Tab title="Chat" className="h-full dark:bg-neutral-800">
-            <Chat/>
-          </Tab>
-          <Tab title="Summary" className="h-full dark:bg-neutral-800">
-            <Summary summary={"Getting Summary"}/>
-          </Tab>
-          <Tab title="Quiz" className="h-full dark:bg-neutral-800">
-            <Quiz/>
-          </Tab>
+      <Tabs variant="light" fullWidth className="border rounded-xl dark:border-neutral-700 bg-absolute_white dark:bg-black">
+        <Tab title="Chat" className="h-full">
+          <Chat/>
+        </Tab>
+        <Tab title="Summary" className="h-full">
+          <Summary summary={"Getting Summary"}/>
+        </Tab>
+        <Tab title="Quiz" className="h-full">
+          <Quiz/>
+        </Tab>
       </Tabs>
     </div>  
   );
