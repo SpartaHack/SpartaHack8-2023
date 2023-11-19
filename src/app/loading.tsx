dca307e590059a0a86b34b7c59586b3c10e439e9
achyutbenz19 <achyut.benz@gmail.com>
2023-11-18 22:04:16 -0500

    spinner color

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Loading = () => {
   return (
     <div className='h-screen w-full flex items-center justify-center'>
-      <Spinner/>
+      <Spinner color='current'/>
     </div>
   )
 }
