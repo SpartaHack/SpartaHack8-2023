1e0622feaf83c675bdb35a4ac8fe1f35248f2c00
achyutbenz19 <achyut.benz@gmail.com>
2023-11-19 22:02:12 -0500

    header border + dropshadow css

--- src/ui/header/header.tsx
+++ src/ui/header/header.tsx
@@ -19,27 +19,29 @@ export default function Header() {
   };
 
   return (
-    <Navbar maxWidth="full" isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen} shouldHideOnScroll isBordered className="sticky top-0 z-10 bg-neutral-100 dark:bg-neutral-900 border-b border-neutral-300 dark:border-neutral-700">
-      <div className="flex w-full py-3 items-center justify-between">
-        <div className='md:mr-6 mr-5 h-8'>
-          <NavbarMenuToggle className=""/>
-        </div>
-        <HomeLinkBar/>
-        <div className="hidden lg:flex space-x-5">
-          <Notification/>
-          <Account 
-            name="Achyut Krishna Byanjankar"
-            description='@achyut_benz'
-          />
-        </div>
-      </div>
-
-        <NavbarMenu onClick={handleClick} className='w-[50%] md:w-[25%] overflow-hidden lg:w-[18%] bg-white dark:bg-neutral-900 border-t dark:border-neutral-800 ' motionProps={sideBarMotion}>
-          <div ref={ref}>  
-            <MenuItems/>
+    <div className="border-b dark:border-neutral-800">
+      <Navbar maxWidth="full" isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen} shouldHideOnScroll className="sticky top-0 z-10 bg-neutral-100  dark:bg-neutral-900">
+        <div className="flex w-full py-3 items-center justify-between">
+          <div className='md:mr-6 mr-5 h-8'>
+            <NavbarMenuToggle className=""/>
           </div>
-        </NavbarMenu>
+          <HomeLinkBar/>
+          <div className="hidden lg:flex space-x-5">
+            <Notification/>
+            <Account 
+              name="Achyut Krishna Byanjankar"
+              description='@achyut_benz'
+            />
+          </div>
+        </div>
 
-    </Navbar>
+        <NavbarMenu onClick={handleClick} className='w-[50%] drop-shadow-sm md:w-[25%] overflow-hidden lg:w-[18%] bg-white dark:bg-neutral-900 border-t dark:border-neutral-800 ' motionProps={sideBarMotion}>
+        <div ref={ref}>  
+          <MenuItems/>
+        </div>
+      </NavbarMenu>
+
+      </Navbar>
+    </div>
   );
 }
