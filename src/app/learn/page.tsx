bdec2fe7b103deb0608e0fb6a7a0b8e1fbdef1b8
achyutbenz19 <achyut.benz@gmail.com>
2023-11-25 18:47:36 -0500

    reformat + hooked to resolve hydration

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,47 +1,16 @@
 "use client";
 import Header from "@/ui/header/header";
-import React, { Suspense, useEffect } from "react";
+import React, { Suspense } from "react";
 import Content from "@/components/learn/content/content";
 import Loading from "../loading";
-import { useStore } from "zustand";
-import { useLearnStore } from "@/context/learn-context";
 import { useSearchParams } from "next/navigation";
-import { auth } from "../../../db/firebase";
-import { getContent } from "../api/endpoints";
+import { useLearnContent } from "@/hooks/use-learn-content";
 
 const LearnPage = () => {
-  const setLearnContent = useStore(
-    useLearnStore,
-    (state) => state.setLearnContent,
-  );
   const params = useSearchParams();
   const contentId = params.get("c");
   const spaceId = params.get("s");
-
-  useEffect(() => {
-    if (contentId && auth.currentUser?.uid) {
-      const fetchData = async () => {
-        let response;
-
-        if (spaceId) {
-          response = await getContent(
-            auth.currentUser?.uid!,
-            contentId,
-            spaceId,
-          );
-          if (response && response.data) {
-            response.data.space_id = spaceId;
-          }
-        } else {
-          response = await getContent(auth.currentUser?.uid!, contentId);
-        }
-        if (response?.data) {
-          setLearnContent(response.data);
-        }
-      };
-      fetchData();
-    }
-  }, [contentId, spaceId]);
+  useLearnContent(contentId!, spaceId!);
 
   return (
     <main className='flex flex-col min-h-screen"'>
---
+++ src/hooks/use-learn-content.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useStore } from "zustand";
+import { useLearnStore } from "@/context/learn-context";
+import { auth } from "../../db/firebase";
+import { getContent } from "@/app/api/endpoints";
+
+export const useLearnContent = (contentId: string, spaceId?: string) => {
+  const setLearnContent = useStore(
+    useLearnStore,
+    (state) => state.setLearnContent,
+  );
+
+  useEffect(() => {
+    if (contentId && auth.currentUser?.uid) {
+      const fetchData = async () => {
+        let response;
+
+        if (spaceId) {
+          response = await getContent(
+            auth.currentUser?.uid!,
+            contentId,
+            spaceId,
+          );
+          if (response && response.data) {
+            response.data.space_id = spaceId;
+          }
+        } else {
+          response = await getContent(auth.currentUser?.uid!, contentId);
+        }
+        if (response?.data) {
+          setLearnContent(response.data);
+        }
+      };
+      fetchData();
+    }
+  }, [contentId, spaceId, setLearnContent]);
+};
