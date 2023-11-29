82104a0f3b80f7817dd5487cc595611525c9d6f3
achyutbenz19 <achyut.benz@gmail.com>
2023-11-29 17:44:25 -0500

    minor

--- src/hooks/use-learn-content.tsx
+++ src/hooks/use-learn-content.tsx
@@ -5,60 +5,53 @@ import {
   generateContentSummary,
   getContent,
 } from "@/app/api/endpoints";
-import useStore from "./use-store";
 import { useLearnStore } from "@/context/learn-context";
 
 export const useLearnContent = (contentId: string, spaceId?: string) => {
   const [loading, setLoading] = useState(false);
   const [fetched, setFetched] = useState(false);
-  const learnContent = useStore(useLearnStore, (state) => state.learnContent);
   const { updateLearnContent, setLearnContent } = useLearnStore();
 
   useEffect(() => {
     const fetchData = async () => {
       if (contentId && auth.currentUser?.uid && !fetched) {
         setLoading(true); 
-        let response;
-        if (!spaceId) {
-          response = await getContent(auth.currentUser?.uid!, contentId);
-        } else {
-          response = await getContent(auth.currentUser?.uid!, contentId, spaceId);
-          if (response && response.data) {
-            response.data.space_id = spaceId;
+        try {
+          let response;
+          if (!spaceId) {
+            response = await getContent(auth.currentUser?.uid!, contentId);
+          } else {
+            response = await getContent(auth.currentUser?.uid!, contentId, spaceId);
+            if (response && response.data) {
+              response.data.space_id = spaceId;
+            }
           }
-        }
-        setLearnContent!(response?.data);
-        if (
-          (!response?.data?.generations?.questions ||
-            !response?.data?.generations?.summary)
-        ) {
-          setLoading(true)
-          const summaryResponse = await generateContentSummary(
-            auth.currentUser?.uid!,
-            contentId,
-          );
-          const questionsResponse = await generateContentQuestions(
-            auth.currentUser?.uid!,
-            contentId,
-          );
+          setLearnContent!(response?.data);
+          if (!response?.data?.generations?.questions || !response?.data?.generations?.summary) {
+            const summaryResponse = await generateContentSummary(auth.currentUser?.uid!, contentId);
+            const questionsResponse = await generateContentQuestions(auth.currentUser?.uid!, contentId);
 
-          const summary = summaryResponse?.data;
-          const questions = questionsResponse?.data;
+            const summary = summaryResponse?.data;
+            const questions = questionsResponse?.data;
 
-          updateLearnContent({
-            generations: {
-              summary,
-              questions,
-            },
-          });
-          setLoading(false)
+            updateLearnContent({
+              generations: {
+                summary,
+                questions,
+              },
+            });
+          }
+        } catch (error) {
+          console.error("An error occurred while fetching data:", error);
+        } finally {
+          setLoading(false);
+          setFetched(true);
         }
-        setLoading(false);
-        setFetched(true);
       };
     }
-    fetchData();
-  }, [contentId, spaceId, learnContent, updateLearnContent, fetched]);
 
-  return { loading };
+    fetchData();
+  }, [contentId, auth.currentUser?.uid]);
+
+return { loading };
 };
