086b11f49688bf3f1d93d27128f2e6134fc3f8f7
achyutbenz19 <achyut.benz@gmail.com>
2023-12-03 13:04:16 -0500

    minor err boundaries

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,21 +3,25 @@ import { CustomButton } from "@/helpers/custom-btn";
 import YouLearnLogo from "@/icon/youlearn-logo";
 import { useRouter } from "next/navigation";
 
-export default function Error({}: {
+export default function Error({ error, reset }: {
   error: Error & { digest?: string };
   reset: () => void;
 }) {
   const router = useRouter();
 
+  const handleReset = () => {
+    reset();
+    router.push("/");
+  }
+
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center">
-      <span className="text-2xl mb-10">An error occured.</span>
-      <YouLearnLogo size="lg" width={350} height={350} />
+      <span className="text-2xl mb-10">An error occurred.</span>
       <CustomButton
         title="Go Back"
         btnType="button"
-        btnStyling="mt-10 text-xl"
-        popOverClickEvent={() => router.push("/")}
+        btnStyling="mt-10 text-xl w-[10%]"
+        clickEvent={handleReset}
       />
     </div>
   );
