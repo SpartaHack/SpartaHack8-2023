975336420f546813679866308aa908ac54fc5140
1davidyu1 <yudavid@msu.edu>
2023-12-05 16:40:52 -0500

    error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,6 +2,7 @@
 import { CustomButton } from "@/helpers/custom-btn";
 import YouLearnLogo from "@/icon/youlearn-logo";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Error({
   error,
@@ -19,11 +20,18 @@ export default function Error({
 
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center">
-      <span className="text-2xl mb-10">An error occurred.</span>
+       <Image
+              src="errorIcon.svg"
+              alt="YouLearn"
+              width={300}
+              height={300}
+        />
+      <span className="text-4xl font-sans font-bold mb-4 ">404 Error</span>
+      <span className="text-xl font-sans font-semibold mb-4 ">Sorry, error has occurred.</span>
       <CustomButton
-        title="Go Back"
+        title="Back to home"
         btnType="button"
-        btnStyling="mt-10 text-xl w-[10%]"
+        btnStyling="mt-10 h-[50.5px] text-md font-black w-[60%] md:w-[20%] bg-black text-white mb-24"
         clickEvent={handleReset}
       />
     </div>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,39 @@
+"use client";
+import { CustomButton } from "@/helpers/custom-btn";
 import YouLearnLogo from "@/icon/youlearn-logo";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
-export default function NotFound() {
+export default function NotFound({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  const handleReset = () => {
+    reset();
+    router.push("/");
+  };
+
   return (
-    <div className="flex flex-col h-screen text-center justify-center">
-      <div className="items-center justify-center">
-        <YouLearnLogo size="lg" width={200} height={200} />
-        <h1 className="text-[50px]">Return</h1>
-      </div>
+    <div className="h-screen w-full flex flex-col justify-center items-center">
+       <Image
+              src="errorIcon.svg"
+              alt="YouLearn"
+              width={300}
+              height={300}
+        />
+      <span className="text-4xl font-sans font-bold mb-4 ">404 Error</span>
+      <span className="text-xl font-sans font-semibold mb-4 ">Sorry, page not found.</span>
+      <CustomButton
+        title="Back to home"
+        btnType="button"
+        btnStyling="mt-10 h-[50.5px] text-md font-black w-[60%] md:w-[20%] bg-black text-white mb-24"
+        clickEvent={handleReset}
+      />
     </div>
   );
 }
