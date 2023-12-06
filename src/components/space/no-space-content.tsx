45a6161614c6c13f73301d545d4746c650ff5622
1davidyu1 <yudavid@msu.edu>
2023-12-06 16:38:05 -0500

    add content empty css

--- src/components/space/no-space-content.tsx
+++ src/components/space/no-space-content.tsx
@@ -10,15 +10,15 @@ const NoSpaceContent = () => {
       {contents && contents.contents.length == 0 && (
         <div className="flex w-full mt-24 flex-col items-center justify-center">
           <div className="flex flex-row">
-            <h1 className="text-2xl">Add a content to this space</h1>
+            <h1 className="text-2xl font-sans font-semibold mr-1">Add a content to this space</h1>
             <Icon
-              icon="fluent:arrow-sprint-16-regular"
-              className="w-8 h-8"
+              icon="fluent:arrow-sprint-20-filled"
+              className="w-8 h-8 font-black"
               style={{ transform: "rotate(-30deg)" }}
             />
           </div>
-          <h2 className="font-light mt-4 text-neutral-500">
-            Learn in half the time.
+          <h2 className="font-sans mt-4 text-neutral-500 text-center">
+            Learn twice as fast and personalize with YouLearn. <br/> Avoid the tedious convention of just reading and watching.
           </h2>
         </div>
       )}
