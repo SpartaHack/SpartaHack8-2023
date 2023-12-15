c22ad1d6022a7f122068d55de154b2c4a6b7f5b8
achyutbenz19 <achyut.benz@gmail.com>
2023-12-14 22:56:27 -0500

    reformat

--- src/icon/youlearn-logo.tsx
+++ src/icon/youlearn-logo.tsx
@@ -8,39 +8,39 @@ const YouLearnLogo = ({ size, tier, height, width }: YouLearnLogoProps) => {
   const router = useRouter();
   const returnHome = () => {
     localStorage.setItem("historyLoading", "true");
-    router.push('/')
-  }
+    router.push("/");
+  };
   return (
     <div className="flex flex-row justify-between">
       {size === "lg" && (
         <>
-            <Image
-              src="youlearn.svg"
-              alt="YouLearn"
-              className="dark:hidden cursor-pointer"
-              width={width ? width : 110}
-              height={height ? height : 110}
-              onClick={returnHome}
-            />
-            <Image
-              src="youlearnDark.svg"
-              alt="YouLearn"
-              className="dark:block cursor-pointer hidden"
-              width={width ? width : 110}
-              height={height ? height : 110}
-              onClick={returnHome}
-            />
+          <Image
+            src="youlearn.svg"
+            alt="YouLearn"
+            className="dark:hidden cursor-pointer"
+            width={width ? width : 110}
+            height={height ? height : 110}
+            onClick={returnHome}
+          />
+          <Image
+            src="youlearnDark.svg"
+            alt="YouLearn"
+            className="dark:block cursor-pointer hidden"
+            width={width ? width : 110}
+            height={height ? height : 110}
+            onClick={returnHome}
+          />
         </>
       )}
       {size === "sm" && (
-          <Image
-            src="youlearnMedia.svg"
-            alt="YouLearnMedia"
-            className="cursor-pointer"
-            width={width ? width : 35}
-            height={height ? height : 35}
-            onClick={returnHome}
-          />
+        <Image
+          src="youlearnMedia.svg"
+          alt="YouLearnMedia"
+          className="cursor-pointer"
+          width={width ? width : 35}
+          height={height ? height : 35}
+          onClick={returnHome}
+        />
       )}
     </div>
   );
