b873608a28c4ec5840300762b3454dde524dad8e
achyutbenz19 <achyut.benz@gmail.com>
2023-12-21 23:05:28 -0600

    Add content dialog box feels too wide

--- src/ui/header/add-content.tsx
+++ src/ui/header/add-content.tsx
@@ -59,7 +59,7 @@ const AddContent = () => {
   return (
     <>
       <CustomModal
-        size="4xl"
+        size="2xl"
         title={
           <div className="shadow-xl rounded-2xl px-4 py-3 bg-black cursor-pointer dark:bg-white text-white dark:text-black dark:white font-semibold font-sans flex flex-row">
             <Icon icon="mi:add" className="w-4 h-4 mt-0.5 mr-1" />
