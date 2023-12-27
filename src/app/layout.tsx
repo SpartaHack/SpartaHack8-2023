97df0bd7dff57857b752e43d547624fab307026d
achyutbenz19 <achyut.benz@gmail.com>
2023-12-27 16:07:46 -0600

    opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,6 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image"
   },
-  openGraph: {
-    images: '/opengraph-image.png',
-  },
 };
 
 export default function RootLayout({
