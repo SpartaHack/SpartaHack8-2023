64a19e2e767910a86dcfdf93f7ae498b53be6b09
achyutbenz19 <achyut.benz@gmail.com>
2023-12-28 14:44:04 -0600

    css fix pdf

--- src/components/learn/content/pdf.tsx
+++ src/components/learn/content/pdf.tsx
@@ -4,6 +4,7 @@ import { useStore } from "zustand";
 
 const PDF = () => {
   const learnContent = useStore(useLearnStore, (state) => state.learnContent);
+
   let source;
   if (learnContent?.source) {
     source = parseInt(learnContent?.source!);
@@ -15,13 +16,12 @@ const PDF = () => {
   return (
     <div className="lg:w-[70%] w-full items-center justify-center">
       <div className="h-[75vh] lg:h-screen">
-        <iframe
-          key={pdfUrl}
-          src={pdfUrl}
-          width="100%"
-          height="85%"
-          className="rounded-xl border-none"
-        />
+      <iframe
+        key={pdfUrl}
+        src={pdfUrl}
+        width="100%"
+        className="rounded-xl border-none h-full lg:h-[85%]"
+      />
       </div>
     </div>
   );
