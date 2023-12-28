9a6163d05504f5520d2edc26a3110efc15c637cb
achyutbenz19 <achyut.benz@gmail.com>
2023-12-28 11:14:15 -0600

    clean ups

--- src/ui/header/space-header.tsx
+++ src/ui/header/space-header.tsx
@@ -27,7 +27,7 @@ const SpaceHeader = () => {
     return <Loading />;
   }
 
-  const spaceName = contents.space ? contents.space.name : "History";
+  const spaceName = contents.space.name;
 
   const handleIconClick = () => {
     setSpaceNameInput(spaceName);
@@ -91,19 +91,15 @@ const SpaceHeader = () => {
             ) : (
               spaceName
             )}
-            {spaceName !== "History" && !editSpaceName && (
               <Icon
                 icon="lucide:pen"
                 className="opacity-0 h-4 w-4 mt-5 ml-2 cursor-pointer group-hover:opacity-50"
                 onClick={handleIconClick}
               />
-            )}
           </div>
-          {spaceName !== "History" && (
-            <div className="flex flex-row my-6  md:my-0 space-between">
-              <AddContent />
-            </div>
-          )}
+          <div className="flex flex-row my-6  md:my-0 space-between">
+            <AddContent />
+          </div>
         </div>
       </div>
       <div className="border-[.5px] sm:mx-24 mx-10 mt-8 dark:border-neutral-800" />
