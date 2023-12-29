bf2f0b9144f03a517eec861f658a22c51940b8e3
achyutbenz19 <achyut.benz@gmail.com>
2023-12-29 09:18:50 -0600

    css

--- src/components/dashboard/not-signed-in.tsx
+++ src/components/dashboard/not-signed-in.tsx
@@ -11,7 +11,7 @@ const NotSignedIn = () => {
         <div className="flex w-full mt-8 px-10 flex-col items-center justify-center">
           <div className="flex space-y-4 w-[12rem] items-center justify-center">
             <CustomButton
-              title={<h1 className="font-bold">Sign in</h1>}
+              title={<h1 className="font-sans">Sign in</h1>}
               btnType="button"
               btnStyling="dark:bg-white bg-black text-white py-2 dark:text-black font-bold flex items-center justify-center rounded-xl h-[50px] mt-4"
               clickEvent={() => router.push("/signin")}
