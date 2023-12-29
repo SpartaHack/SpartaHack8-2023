22e505227b8ce3ebde83cda6a407128cce970806
achyutbenz19 <achyut.benz@gmail.com>
2023-12-29 09:01:05 -0600

    verify mobile

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -40,18 +40,18 @@ const Verify = () => {
     <>
       <SecondaryHeader />
       <div className="flex bg-absolute_white dark:bg-black flex-col h-screen items-center justify-center">
-        <div className="w-full sm:w-3/5 flex p-8 space-y-4 max-w-lg items-center justify-center">
+        <div className="w-full flex p-8 space-y-4 max-w-lg items-center justify-center">
           <div className="p-8 space-y-4 max-w-md w-full">
-            <h1 className="text-3xl text-left font-black text-black dark:text-white">
+            <h1 className="text-3xl md:text-left text-center font-black text-black dark:text-white">
               Verify Your Account
             </h1>
-            <p className="text-md text-left font-thin text-neutral-500 dark:text-neutral-300">
+            <p className="text-md md:text-left text-center font-thin text-neutral-500 dark:text-neutral-300">
               Check your mailbox to verify your email.
             </p>
             <CustomButton
               title={<h1 className="font-bold">Resend Email</h1>}
               btnType="button"
-              btnStyling="dark:bg-white bg-black text-white py-2 dark:text-black font-bold flex items-center justify-center rounded-xl h-[50px] w-full mt-4"
+              btnStyling="dark:bg-white bg-black text-white py-2 dark:text-black font-bold flex items-center justify-center rounded-xl h-[50px] md:w-full mt-4"
               clickEvent={resendEmail}
             />
           </div>
