df33fa533080a52ba949dc31a9563a3bcc5aaa9c
achyutbenz19 <achyut.benz@gmail.com>
2024-01-02 11:47:09 -0600

    upgrade -> pricing

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import FAQ from "@/components/upgrade/faq";
-import Upgrade from "@/components/upgrade/upgrade";
+import FAQ from "@/components/pricing/faq";
+import Upgrade from "@/components/pricing/upgrade";
 import ErrorMessage from "@/helpers/error-message";
 import Footer from "@/ui/footer/footer";
 import Header from "@/ui/header/header";
