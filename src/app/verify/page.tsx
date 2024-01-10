22fa5c6305fde8452971ad28e495d3a47975f17e
1davidyu1 <yudavid@msu.edu>
2024-01-10 15:12:24 -0500

    verify "junk" email

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -6,6 +6,7 @@ import { CustomButton } from "@/helpers/custom-btn";
 import { auth } from "../../../db/firebase";
 import { sendEmailVerification } from "firebase/auth";
 import { toast } from "sonner";
+import Link from "next/link";
 
 const Verify = () => {
   const router = useRouter();
@@ -42,16 +43,16 @@ const Verify = () => {
       <div className="flex bg-absolute_white dark:bg-black flex-col h-screen items-center justify-center">
         <div className="w-full flex p-8 space-y-4 max-w-lg items-center justify-center">
           <div className="p-8 space-y-4 max-w-md w-full">
-            <h1 className="text-3xl md:text-left text-center font-black text-black dark:text-white">
+            <h1 className="text-3xl text-center font-black text-black dark:text-white">
               Verify Your Account
             </h1>
-            <p className="text-md md:text-left text-center font-thin text-neutral-500 dark:text-neutral-300">
-              Check your mailbox to verify your email.
+            <p className="text-md text-center font-thin text-neutral-500 dark:text-neutral-300">
+              Check your inbox or junk email to verify your account.
             </p>
             <CustomButton
               title={<h1 className="font-bold">Resend Email</h1>}
               btnType="button"
-              btnStyling="dark:bg-white bg-black text-white py-2 dark:text-black font-bold flex items-center justify-center rounded-xl h-[50px] md:w-full mt-4"
+              btnStyling="dark:bg-white bg-black text-white py-2 dark:text-black font-bold flex items-center justify-center rounded-xl h-[50px] md:w-full mt-8"
               clickEvent={resendEmail}
             />
           </div>
