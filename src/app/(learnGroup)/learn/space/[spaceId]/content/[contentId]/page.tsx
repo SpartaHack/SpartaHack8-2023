9ad656197bbeffba4f34277fd55c2648429db21e
achyutbenz19 <achyut.benz@gmail.com>
2024-01-12 00:44:17 -0500

    minor group routes

--- src/app/(learnGroup)/learn/space/[spaceId]/content/[contentId]/page.tsx
+++ src/app/(learnGroup)/learn/space/[spaceId]/content/[contentId]/page.tsx
@@ -1,4 +1,3 @@
-import Header from "@/ui/header/header";
 import React from "react";
 import Content from "@/components/learn/content/content";
 
@@ -8,10 +7,7 @@ const LearnPage = ({
   params: { spaceId: string; contentId: string };
 }) => {
   return (
-    <main className="flex flex-col min-h-screen bg-neutral-100 dark:bg-neutral-900">
-      <Header />
       <Content spaceId={params.spaceId} contentId={params.contentId} />
-    </main>
   );
 };
 
