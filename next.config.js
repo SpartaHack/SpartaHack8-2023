b58aaaec28d1ce495b921fd67cceaaeae62f6ca1
achyutbenz19 <achyut.benz@gmail.com>
2024-01-15 16:10:36 -0500

    react pdf viewer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 const million = require("million/compiler");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, webpack }
+  ) => {
+    config.resolve.alias.canvas = false
+    config.resolve.alias.encoding = false
+    return config
+  },
   images: {
     // domains: [
     //   "i.ytimg.com",
--- src/components/learn/content/pdf.tsx
+++ src/components/learn/content/pdf.tsx
@@ -1,27 +1,42 @@
+"use client"
 import { useLearnStore } from "@/context/learn-context";
-import React from "react";
+import { Worker } from "@react-pdf-viewer/core";
+import React, { useEffect } from "react";
 import { useStore } from "zustand";
+import { Viewer } from '@react-pdf-viewer/core';
+import { pageNavigationPlugin } from '@react-pdf-viewer/page-navigation';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/page-navigation/lib/styles/index.css';
+import { SpecialZoomLevel } from '@react-pdf-viewer/core';
 
 const PDF = () => {
+  const pageNavigationPluginInstance = pageNavigationPlugin();
   const learnContent = useStore(useLearnStore, (state) => state.learnContent);
 
-  let source;
+  let source: number;
   if (learnContent?.source) {
     source = parseInt(learnContent?.source!);
   } else {
     source = 0;
   }
-  const pdfUrl = learnContent?.metadata.content_url + `#page=${source}`;
+  const pdfUrl = learnContent?.metadata.content_url;
+
+  useEffect(() => {
+    pageNavigationPluginInstance.jumpToPage(source - 1); 
+  }, [source]);
 
   return (
     <div className="lg:w-[70%] w-full items-center justify-center">
-      <div className="h-[75vh] lg:h-screen">
-        <iframe
-          key={pdfUrl}
-          src={pdfUrl}
-          width="100%"
-          className="rounded-xl border-none h-full lg:h-[85%]"
-        />
+      <div className="h-[75vh] lg:h-screen rounded-xl overflow-hidden">
+        <div className="rounded-xl h-full lg:h-[85.3%] overflow-hidden">
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+            <Viewer
+              defaultScale={SpecialZoomLevel.PageFit} 
+              plugins={[pageNavigationPluginInstance]}
+              fileUrl={pdfUrl!}
+            />
+          </Worker>
+        </div>
       </div>
     </div>
   );
