e074e02c2f2117e68a9114efc8178748ab7fc1e1
achyutbenz19 <achyut.benz@gmail.com>
2024-01-20 21:55:13 -0500

    minor

--- src/ui/header/space-header.tsx
+++ src/ui/header/space-header.tsx
@@ -58,7 +58,7 @@ const SpaceHeader = () => {
         contents.space._id,
         spaceNameInput,
         contents.space.description,
-        "private",
+        contents.space.visibility
       );
       if (response) {
         useSpaceStore.getState().updateSpaceData(updatedData);
