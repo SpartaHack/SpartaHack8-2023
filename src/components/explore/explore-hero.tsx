984c35ef781f7bffd21426e3281f4c7d621562ee
achyutbenz19 <achyut.benz@gmail.com>
2024-02-02 21:56:54 -0500

    minor

--- src/components/explore/explore-hero.tsx
+++ src/components/explore/explore-hero.tsx
@@ -3,13 +3,13 @@ import ExploreCarousel from "./explore-carousel";
 
 const ExploreHero = () => {
   return (
-    <div className="w-full bg-neutral-900 h-[490px] pt-6">
+    <div className="w-full bg-neutral-900 h-[480px] pt-6">
       <div className="mx-6">
         <h1 className="text-white text-2xl pb-1">Explore your curiosity</h1>
         <span className="text-neutral-500 text-sm">
           Learn from the best, and create yours too
         </span>
-        <div className="mt-4 pr-10 w-full flex flex-row justify-center">
+        <div className="mt-5 pr-10 w-full flex flex-row justify-center">
           <ExploreCarousel />
         </div>
       </div>
