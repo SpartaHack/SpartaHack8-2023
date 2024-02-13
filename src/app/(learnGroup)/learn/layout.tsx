a2125b579a7ab45b71982b8447d549c341b5fc45
achyutbenz19 <achyut.benz@gmail.com>
2024-02-13 11:52:16 -0500

    fetch opt

--- src/app/(learnGroup)/learn/layout.tsx
+++ src/app/(learnGroup)/learn/layout.tsx
@@ -1,15 +1,12 @@
-import Loading from "@/app/loading";
 import Header from "@/ui/header/header";
-import React, { Suspense } from "react";
+import React from "react";
 
 const learnGroupLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Suspense fallback={<Loading />}>
       <main className="flex flex-col min-h-screen bg-neutral-100 dark:bg-neutral-900">
         <Header />
         {children}
       </main>
-    </Suspense>
   );
 };
 
