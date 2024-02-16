ef302e0a79e0bfc4acb64187e758484bd84746a2
achyutbenz19 <achyut.benz@gmail.com>
2024-02-16 10:52:09 -0500

    fixes

--- src/ui/header/add-content.tsx
+++ src/ui/header/add-content.tsx
@@ -97,7 +97,6 @@ const AddContent = () => {
     "https://youtu.be/kqtD5dpn9C8",
     "https://youtube.com/playlist?list=PLZHQObO...",
     "https://arxiv.org/pdf/1706.03762.pdf",
-    "What are black holes?",
     "https://mediaspace.stanford.edu/media/...i257wd8",
   ];
 
